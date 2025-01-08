254d261059cf1f65b23c20e46fe7f74b02243390
kimocoder <christian@aircrack-ng.org>
2025-01-08 07:46:18 +0100

    complete dr5018 support & add wwan feed

--- target/linux/qualcommax/image/ipq50xx.mk
+++ target/linux/qualcommax/image/ipq50xx.mk
@@ -32,15 +32,13 @@ endef
 TARGET_DEVICES += linksys_mx5500
 
 define Device/wallys_dr5018
-	SOC := ipq5018
-	DEVICE_MODEL := Wallys
-	DEVICE_VARIANT := DR5018
-	BLOCKSIZE := 128k
-	PAGESIZE := 2048
-	IMAGE_SIZE := 129408k
-	UBINIZE_OPTS := -E 5
-	IMAGES := factory.bin
-	IMAGE/factory.bin := append-ubi | check-size | ubinize-image -s 2048
-	DEVICE_PACKAGES := kmod-qca-nss-dp kmod-usb3 kmod-usb-dwc3 kmod-usb-dwc3-qcom kmod-usb-phy
+	DEVICE_TITLE := Wallys DR5018
+	DEVICE_DTS := ipq5018-dr5018
+	SUPPORTED_DEVICES := wallys,dr5018
+	DEVICE_PACKAGES := ath11k-wifi-wallys-dr5018 uboot-envtools ath11k-firmware-qcn6122
+	DEVICE_DTS_CONFIG := config@mp03.5-c1
+	KERNEL = kernel-bin | libdeflate-gzip | fit gzip $(KDIR)/image-ipq5018-dr5018.dtb
+	IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+	IMAGE/sysupgrade.bin/squashfs := append-rootfs | sysupgrade-tar rootfs=$$$$@ | append-metadata
 endef
 TARGET_DEVICES += wallys_dr5018
--- target/linux/qualcommax/ipq50xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq50xx/base-files/lib/upgrade/platform.sh
@@ -5,22 +5,43 @@ RAMFS_COPY_BIN='fw_printenv fw_setenv head'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
 
 platform_check_image() {
-	return 0;
+    # Validate image for supported boards
+    case "$(board_name)" in
+    linksys,mx2000|\
+    linksys,mx5500|\
+    wally,dr5018)
+        return 0
+        ;;
+    *)
+        echo "Unsupported board: $(board_name)"
+        return 1
+        ;;
+    esac
 }
 
 platform_do_upgrade() {
-	case "$(board_name)" in
-	linksys,mx2000|\
-	linksys,mx5500)
-		platform_do_upgrade_linksys "$1"
-		;;
-        wally,dr5018)
-                CI_KERNPART="0:HLOS"
-                CI_ROOTPART="rootfs"
-                emmc_do_upgrade "$1"
-                ;;
-	*)
-		default_do_upgrade "$1"
-		;;
-	esac
+    CI_UBIPART="rootfs"
+    CI_ROOTPART="ubi_rootfs"
+    CI_IPQ807X=1
+
+    case "$(board_name)" in
+    linksys,mx2000|\
+    linksys,mx5500)
+        platform_do_upgrade_linksys "$1"
+        ;;
+    wally,dr5018)
+        if [ -f /proc/boot_info/rootfs/upgradepartition ]; then
+            CI_UBIPART="$(cat /proc/boot_info/rootfs/upgradepartition)"
+            CI_BOOTCFG=1
+        else
+            echo "Error: Upgrade partition file not found!"
+            return 1
+        fi
+        nand_upgrade_tar "$1"
+        ;;
+    *)
+        echo "Warning: Falling back to default upgrade method."
+        default_do_upgrade "$1"
+        ;;
+    esac
 }
